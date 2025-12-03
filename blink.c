8bf4430644219bbf74c96635d98e4b55e9301eab
HarutoImamura <haruto.i1999@gmail.com>
2025-12-03 22:47:15 +0900

    outLED blink

--- blink.c
+++ blink.c
@@ -1,6 +1,7 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+#define LED_PIN 15
 #define LED_DELAY_MS 500
 
 
@@ -17,6 +18,9 @@ int main() {
     int rc = pico_led_init();
     stdio_init_all();
     hard_assert(rc == PICO_OK);
+    // GP15を出力モードに設定
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, true);
 
     while (true) {
         printf("Hello, Pico 2 W!\n");
@@ -24,5 +28,12 @@ int main() {
         sleep_ms(LED_DELAY_MS);      // 500ms待つ
         pico_set_led(false);         // LED OFF
         sleep_ms(LED_DELAY_MS);      // 500ms待つ
+
+        printf("LED ON\n");
+        gpio_put(LED_PIN, true);   // LED点灯
+        sleep_ms(LED_DELAY_MS);
+        printf("LED OFF\n");
+        gpio_put(LED_PIN, false);  // LED消灯
+        sleep_ms(LED_DELAY_MS);
     }
 }

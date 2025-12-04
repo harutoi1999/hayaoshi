dbe2d783660fee8975b264270dfb9d4d51186ef5
HarutoImamura <haruto.i1999@gmail.com>
2025-12-04 22:18:27 +0900

    圧電スピーカーと基板LEDを一定間隔で

--- blink.c
+++ blink.c
@@ -1,5 +1,6 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/pwm.h"
 
 #define LED_PIN 15
 #define LED_DELAY_MS 500
@@ -14,26 +15,38 @@ void pico_set_led(bool led_on) {
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 }
 
+// 周波数とデューティ比をセットする関数
+void pwm_set_freq(uint slice, uint channel, uint freq, float duty) {
+    uint32_t clock = 125000000; // 125MHz
+    float divider = 16.0f;      // ← 分周を大きくする
+    uint32_t wrap = (clock / divider / freq) - 1;
+
+    pwm_set_clkdiv(slice, divider);
+    pwm_set_wrap(slice, wrap);
+    pwm_set_chan_level(slice, channel, wrap * duty);
+}
+
 int main() {
     int rc = pico_led_init();
     stdio_init_all();
     hard_assert(rc == PICO_OK);
     // GP15を出力モードに設定
     gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, true);
+    // gpio_set_dir(LED_PIN, true);
+    
+    // GP15 を PWM に設定（スピーカー）
+    gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+    uint slice = pwm_gpio_to_slice_num(LED_PIN);
+    uint chan  = pwm_gpio_to_channel(LED_PIN);
+    pwm_set_enabled(slice, true);
 
     while (true) {
         printf("Hello, Pico 2 W!\n");
+        pwm_set_freq(slice, chan, 523, 0.5f);
         pico_set_led(true);          // LED ON
         sleep_ms(LED_DELAY_MS);      // 500ms待つ
+        pwm_set_freq(slice, chan, 523, 0.0f);
         pico_set_led(false);         // LED OFF
         sleep_ms(LED_DELAY_MS);      // 500ms待つ
-
-        printf("LED ON\n");
-        gpio_put(LED_PIN, true);   // LED点灯
-        sleep_ms(LED_DELAY_MS);
-        printf("LED OFF\n");
-        gpio_put(LED_PIN, false);  // LED消灯
-        sleep_ms(LED_DELAY_MS);
     }
 }
